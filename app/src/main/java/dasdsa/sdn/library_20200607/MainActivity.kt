5f3b499b4ee7d84f86e742ad01ca610c788e8d60
umgonji <umgonji@naver.com>
2020-06-07 11:10:11 +0900

    프사를 누르면 프사 크게 보기  => 줌인/줌아웃 구현 필요   => 더블클릭시 줌인도 구현 필요

--- app/src/main/java/dasdsa/sdn/library_20200607/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/library_20200607/MainActivity.kt
@@ -1,7 +1,10 @@
 package dasdsa.sdn.library_20200607
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,13 @@ class MainActivity : BaseActivity() {
 
 
     override fun setupEvents() {
+
+        profileImg.setOnClickListener {
+            Log.d("메인화면", "프사누름")
+            val myIntent = Intent(mContext, ViewProfileImageActivity::class.java)
+            startActivity(myIntent)
+        }
+
     }
 
     override fun setValues() {
---
+++ app/src/main/java/dasdsa/sdn/library_20200607/ViewProfileImageActivity.kt
@@ -0,0 +1,24 @@
+package dasdsa.sdn.library_20200607
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfileImageActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile_image)
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}

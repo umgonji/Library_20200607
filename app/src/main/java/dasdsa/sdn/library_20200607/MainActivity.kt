52749073ff81e1408b1d1aa8ca7ed2da55058204
umgonji <umgonji@naver.com>
2020-06-07 11:51:05 +0900

    인터넷상의 이미지 불러오고 인터넷 사용을 명시해 줬다. manifests 에 명시함

--- app/src/main/java/dasdsa/sdn/library_20200607/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/library_20200607/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -26,6 +27,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        Glide.with(mContext).load("https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.newdaily.co.kr%2Fsite%2Fdata%2Fhtml%2F2019%2F12%2F27%2F2019122700187.html&psig=AOvVaw1YZDpIARpPkPFL629e3ko7&ust=1591584414172000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjEj5bY7ukCFQAAAAAdAAAAABAD").into(profileImg)
     }
 
 
